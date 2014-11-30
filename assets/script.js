0689c4bba082249d81ccd980c5c7b09ffe7bfb6e
gz95460 <gz95460@berkeley.edu>
2014-11-29 21:10:49 -0800

    please work

---
+++ assets/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+		//Modal-Overlay-1 ( This is really repetitive code --> how can we generalize it?)
+	
+		$('#image-fit-1').click(function() {
+			$('#modal-container-1').show();
+		})
+
+		$('.button').click(function() {
+			$('#modal-container-1').hide();
+		})
+
+		$('#modal-overlay-1').click(function() {
+			$('#modal-container-1').hide();
+		})
+
+		//Modal-Overlay-2 
+
+		$('#image-fit-2').click(function() {
+			$('#modal-container-2').show();
+		})
+
+		$('.button').click(function() {
+			$('#modal-container-2').hide();
+		})
+
+		$('#modal-overlay-2').click(function() {
+			$('#modal-container-2').hide();
+		})
+
+		//Modal-Overlay-3
+
+		$('#image-fit-3').click(function() {
+			$('#modal-container-3').show();
+		})
+
+		$('.button').click(function() {
+			$('#modal-container-3').hide();
+		})
+
+		$('#modal-overlay-3').click(function() {
+			$('#modal-container-3').hide();
+		})
+
+		//Sign Up - 
+
+		$('.content-icon').click(function() {
+			$('#modal-container-1').show();
+		})
+
+		$('.button').click(function() {
+			$('#modal-container-1').hide();
+		})
+
+		$('#modal-overlay-1').click(function() {
+			$('#modal-container-1').hide();
+		})
+
+});
